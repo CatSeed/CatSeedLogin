1570020266edf30c0b289c53db4185b090720dad
Shuiling <1668589925@qq.com>
2024-05-28 10:38:17 +0800

    根据提供的diff信息，新生成的提交信息可以是：
    "优化代码逻辑，修复空指针异常，增强代码健壮性。"

--- src/main/java/cc/baka9/catseedlogin/bukkit/object/LoginPlayerHelper.java
+++ src/main/java/cc/baka9/catseedlogin/bukkit/object/LoginPlayerHelper.java
@@ -12,6 +12,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import org.geysermc.floodgate.api.FloodgateApi;
 
 import java.lang.reflect.InvocationTargetException;
@@ -73,24 +74,29 @@ public class LoginPlayerHelper {
     }
 
     public static boolean isFloodgatePlayer(String name) {
+        if (name == null) {
+            return false; // or handle accordingly based on the requirements
+        }
+
         Player player = Bukkit.getPlayerExact(name);
         return player != null && isFloodgatePlayer(player);
     }
 
     public static boolean isFloodgatePlayer(Player player) {
-        return Bukkit.getPluginManager().getPlugin("floodgate") != null && FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId());
+        Plugin floodgatePlugin = Bukkit.getPluginManager().getPlugin("floodgate");
+        return floodgatePlugin != null && FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId());
     }
 
     // 记录登录IP
-    public static void recordCurrentIP(Player player, LoginPlayer lp){
+    public static void recordCurrentIP(Player player, LoginPlayer lp) {
         String currentIp = player.getAddress().getAddress().getHostAddress();
         List<String> ipsList = lp.getIpsList();
         ipsList.add(currentIp);
         ipsList = ipsList.stream().distinct().collect(Collectors.toList());
         if (ipsList.size() > 5) {
-            ipsList.removeFirst();
+            ipsList.remove(0);
         }
-        lp.setIps(String.join(";", ipsList.toArray(new String[0])));
+        lp.setIps(String.join(";", ipsList.toArray(new String[ipsList.size()])));
         CatSeedLogin.instance.runTaskAsync(() -> {
             try {
                 CatSeedLogin.sql.edit(lp);
@@ -101,7 +107,7 @@ public class LoginPlayerHelper {
     }
 
     // ProtocolLib发包空背包
-    public static void sendBlankInventoryPacket(Player player){
+    public static void sendBlankInventoryPacket(Player player) throws InvocationTargetException {
         ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
         PacketContainer inventoryPacket = protocolManager.createPacket(PacketType.Play.Server.WINDOW_ITEMS);
         inventoryPacket.getIntegers().write(0, 0);
@@ -110,12 +116,10 @@ public class LoginPlayerHelper {
         ItemStack[] blankInventory = new ItemStack[inventorySize];
         Arrays.fill(blankInventory, new ItemStack(Material.AIR));
 
-
         StructureModifier<ItemStack[]> itemArrayModifier = inventoryPacket.getItemArrayModifier();
         if (itemArrayModifier.size() > 0) {
             itemArrayModifier.write(0, blankInventory);
         } else {
-
             StructureModifier<List<ItemStack>> itemListModifier = inventoryPacket.getItemListModifier();
             itemListModifier.write(0, Arrays.asList(blankInventory));
         }
